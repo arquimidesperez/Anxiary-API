1ace81521a20ebd5a47ca9cf5f6b9bce788bd4dd
Arquimides <arquimides@Arquimidess-MacBook-Air.local>
2021-06-07 12:55:08 -0400

    slow start, have a homepage component need to build it out further

---
+++ anxiary-app/src/services/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const apiKey = process.env.REACT_APP_AIRTABLE_KEY;
+
+const url = 'https://api.airtable.com/v0/appdPcv2eTjIOH8ZJ/Table%201';
+
+const config = {
+  headers: {
+    Authorization: `Bearer ${apiKey}`,
+  },
+};
+
+export const getEntries = async () => {
+  try {
+    const response = await axios.get(url, config);
+    console.log(response.data.records);
+  } catch (error) {
+    console.error(error)
+  }
+}
