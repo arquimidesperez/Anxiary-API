e90ebe55b06125a77b2cf81d58a3c7f1847092eb
Arquimides <arquimides@Arquimidess-MacBook-Air.local>
2021-06-08 21:57:36 -0400

    Error

--- anxiary-app/src/components/PastEntries.jsx
+++ anxiary-app/src/components/PastEntries.jsx
@@ -1,21 +1,31 @@
 import { useEffect, useState } from 'react';
+// import { Link } from 'react-router-dom';
 import { getEntries } from '../services/api';
-// // import { Link } from 'react-router-dom';
 
 export default function PastEntries() {
   const [entries, setEntries] = useState([]);
+  // const { id } = useParams;
+  // console.log(id);
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await getEntries();
-      console.log(res);
       setEntries(res);
+      console.log(res);
     };
     fetchData();
   }, []);
 
   return (
-    <div>
+      <div>
+      {entries && entries.map((entry) => {
+        return <div key={entry.id}>
+          {/* <Link to={`/pastentries/${entry.id}`}>{entry.field.Title}</Link> */}
+          <p>{entry.fields.Title}</p>
+          </div>
+      })}
+      <br />
+      <br />
       <br />
       <br />
       <br />
