a49fa403e714d7093fed5194f74a8f578dde9886
Arquimides <arquimides@Arquimidess-MacBook-Air.local>
2021-06-11 10:03:15 -0400

    mvp except calendar

--- anxiary-app/src/components/EditViewEntries.jsx
+++ anxiary-app/src/components/EditViewEntries.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { editEntry, getEntry } from '../services/api';
 
 export default function EditViewEntries() {
--- anxiary-app/src/components/MoodTimeline.jsx
+++ anxiary-app/src/components/MoodTimeline.jsx
@@ -1,17 +1,10 @@
-// import HeatMapCalendar from 'react-monthly-heatmap';
 
 export default function moodTimeline() {
   
   return (
     <div>
 
-{/* <HeatMapCalendar
-  values={[
-    { date: '2019-03-31', count: 1, color: '#8cc665' },
-    { date: '2019-04-01', count: 2, color: '#44a340' },
-    { date: '2019-04-06', count: 3, color: '#1e6823' },
-  ]} */}
-{/* /> */}
+
     </div>
   )
   
