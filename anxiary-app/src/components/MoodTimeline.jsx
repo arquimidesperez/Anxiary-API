200543e8e057ebad4fe0be62201e0f00d9520242
Arquimides <arquimides@Arquimidess-MacBook-Air.local>
2021-06-11 16:26:58 -0400

    calendar implementation, need CSS

--- anxiary-app/src/components/MoodTimeline.jsx
+++ anxiary-app/src/components/MoodTimeline.jsx
@@ -1,11 +1,137 @@
+// import CalendarHeatmap from 'reactjs-calendar-heatmap'
+import { useState, useEffect } from 'react';
+import MonthViewCalender from '../lib/Calendar';
+import { getEntries } from '../services/api';
+import './stylesheets/moodtimeline.css'
 
-export default function moodTimeline() {
+// import MonthViewCalender from "../../Calender/MonthViewCalender";
+// import MonthDay from "../calender/monthDay";
+// import { useState, useEffect } from "react";
+export default function MoodTimeline() {
+  const [count, setCount] = useState(0);
+  const Month = new MonthViewCalender();
+  const [month, setMonth] = useState(Month);
+  const [currentDay, setCurrentDay] = useState(Month.currentDateOfMonth());
+  const [monthArray, setMonthArray] = useState(Month.monthArray());
+  const [monthName, setMonthName] = useState(Month.monthName);
+  const [entries, setEntries] = useState([]);
+
   
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getEntries();
+      setEntries(res);
+      console.log(res);
+    };
+    fetchData();
+  }, []);
+
+  // console.log(monthArray);
+
+
+  useEffect(() => {
+    setMonth(new MonthViewCalender(`moment().add(${count}, 'month')`));
+  }, [count]);
+  useEffect(() => {
+    setMonthArray(month.monthArray());
+    setMonthName(month.monthName);
+  }, [month]);
+  const button = () => {
+      return <button onClick={() => setCount(count - 1)}>&lt;</button>;
+  };
+
+  const hotDay = [];
+  entries.forEach((entry) => {
+    hotDay.push(entry.fields?.Date);
+  })
+
+
+  const displayDate = (day) => {
+    console.log(hotDay);
+    // console.log(day);
+    let found;
+    if (hotDay.includes(day.formattedDate)) {
+      found = entries.find((entry) => {
+        return entry.fields?.Date == day.formattedDate
+      });
+      console.log(found);
+    }
+    if (found) {
+      return <div className="Day"> {day.date} + {found.fields.Mood} </div>
+    } else {
+      return <div className="Day"> {day.date} </div>      
+    }
+  }
+
+  // <div className="flex flex-wrap m-1 md:m-4 md:mt-10 sm:p-1 rounded border-solid border md:border-2 md:border-gray-400 md:shadow-lg">
   return (
-    <div>
-
-
+    <div className="CalendarHolder">
+      <div className="w-full">
+        <div className="MonthContainer">
+          <p>
+          {button()}      
+          {monthName}      
+          <button onClick={() => setCount(count + 1)}>&gt;</button>
+          </p>
+        </div>
+        <div className="flex text-sm text-center">
+          <div className="WeekDay">Sunday</div>
+          <div className="WeekDay">Monday</div>
+          <div className="WeekDay">Tuesday</div>
+          <div className="WeekDay">Wednesday</div>
+          <div className="WeekDay">Thursday</div>
+          <div className="WeekDay">Friday</div>
+          <div className="WeekDay">Saturday</div>
+        </div>
+      </div>
+      {monthArray.map((day, index) => {
+        return (displayDate(day))
+        // <MonthDay
+        //   day={day}
+        //   today={currentDay}
+        //   monthName={monthName}
+        //   key={index}
+        //   index={index}
+        // />
+      })}
     </div>
-  )
-  
+  );
 }
+
+// export default function MoodTimeline() {
+//   const calendar = new MonthViewCalendar();
+//   const [count, setCount] = useState(0);
+//   const [currentMonth, setCurrentMonth] = useState(calendar);
+//   const [monthName, setMonthName] = useState(calendar.monthName);
+//   const [monthArray, setMonthArray] = useState(calendar.monthArray());
+//   console.log(calendar);
+//   console.log(calendar.monthArray());
+
+//   // useEffect(() => {
+//   //   setCurrentMonth(new MonthViewCalendar(`moment().add(${count}, 'month')`));
+//   // }, [count]);
+
+//   // useEffect(() => {
+//   //   setMonthArray(calendar.monthArray());
+//   //   setMonthName(calendar.monthName);
+//   // }, [currentMonth]);
+
+
+//   return (
+//     <div>
+//             <br />
+//       <br />
+//       <br />
+//       <button onClick={() => { setCount(prevCount => prevCount - 1) }}> - </button>
+//       <div>{monthName}</div>
+//       <button onClick={() => { setCount(prevCount => prevCount + 1) }}> + </button>
+//       {monthArray.map((day) => {
+//         return (
+//           <div>{day.date}</div>
+//         )
+//       })}
+//     </div>
+//   )
+  
+// }
---
+++ anxiary-app/src/lib/Calendar.js
@@ -0,0 +1,59 @@
+const moment = require("moment");
+const TIME = moment();
+class MonthViewCalender {
+  constructor(month = TIME) {
+    this.now = typeof month == "string" ? eval(month) : month;
+    this.monthName = this.now.format("MMMM");
+    this.monthNumber = this.now.format("MM");
+    this.yearNumber = this.now.format("YYYY");
+    this.dateOfMonth = typeof month == "string" ? null : this.now.date();
+    this.lengthOfThisMonth = this.now.daysInMonth();
+    this.monthStartWeekDay = this.now.date(1).weekday();
+    this.monthEndWeekDay = this.now.date(this.lengthOfThisMonth).weekday();
+    this.weekday = this.now.weekday();
+    this.lengthOfLastMonth = this.now.subtract(1, "month").daysInMonth();
+  }
+  // builds an array of date objects
+  monthArray() {
+    let month = this.monthNumber;
+    let year = this.yearNumber;
+    let length = this.lengthOfLastMonth;
+    let start = this.monthStartWeekDay;
+    let difference = length - start;
+    let array = [];
+    // Add dates of last month to array
+    if (start != 0) {
+      for (let v = difference + 1; v <= length; v++) {
+        array.push(this.buildDateObject(v, -1));
+      }
+    }
+    for (let v = 1; v <= this.lengthOfThisMonth; v++) {
+      array.push(this.buildDateObject(v, 0, month, year));
+    }
+    // adds dates of next month to array
+    if (this.monthEndWeekDay != 6) {
+      let d = this.monthEndWeekDay;
+      let count = 1;
+      for (let v = d; v < 6; v++) {
+        array.push(this.buildDateObject(count, +1));
+        count += 1;
+      }
+    }
+    return array;
+  }
+  buildDateObject(day = "", last = 0, month, year) {
+    //2021-06-01
+    let obj = {};
+    if (day < 10) {
+      day = `0${day}`
+    }
+    obj.date = day;
+    obj.month = last;
+    obj.formattedDate = `${year}-${month}-${day}`;
+    return obj;
+  }
+  currentDateOfMonth() {
+    return moment().date();
+  }
+}
+module.exports = MonthViewCalender;
